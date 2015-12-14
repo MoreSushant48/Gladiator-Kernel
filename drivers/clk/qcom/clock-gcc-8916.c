a28fdc51e54e5c3f5b60cf7db1938c0724dd03f1
Shoaib0597 <Shoaib0595@gmail.com>
2015-12-14 17:29:44 +0530

    UC and OC GPU

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -582,7 +582,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
-	F( 100000000,  gpll0_aux,   8,	  0,	0),
+	F(  50000000,  gpll0_aux,   8,	  0,	0),
 	F( 160000000,  gpll0_aux,   5,	  0,	0),
 	F( 177780000,  gpll0_aux, 4.5,	  0,	0),
 	F( 200000000,  gpll0_aux,   4,	  0,	0),
@@ -590,7 +590,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
-	F( 600000000,      gpll2,   2,	  0,	0),
+	F( 650000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -598,7 +598,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
-	F( 100000000,  gpll0_aux,   8,	  0,	0),
+	F(  50000000,  gpll0_aux,   8,	  0,	0),
 	F( 160000000,  gpll0_aux,   5,	  0,	0),
 	F( 177780000,  gpll0_aux, 4.5,	  0,	0),
 	F( 200000000,  gpll0_aux,   4,	  0,	0),
@@ -606,7 +606,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
-	F( 600000000,      gpll2,   2,    0,    0),
+	F( 650000000,      gpll2,   2,    0,    0),
 	F_END
 };
 
@@ -619,8 +619,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOW, 100000000, NOMINAL, 310000000, HIGH,
-			600000000),
+		VDD_DIG_FMAX_MAP3(LOW, 50000000, NOMINAL, 310000000, HIGH,
+			650000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
