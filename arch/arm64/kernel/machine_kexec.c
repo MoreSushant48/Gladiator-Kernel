d71aeaa1b29d4d0dee43579a4c84fbf57e43876b
MoreSushant48 <moresushant48@gmail.com>
2016-04-02 22:53:44 +0530

    first commit

--- arch/arm64/kernel/machine_kexec.c
+++ arch/arm64/kernel/machine_kexec.c
@@ -9,7 +9,11 @@
  * published by the Free Software Foundation.
  */
 
+/* Bypass purgatory for debugging. */
+static const int bypass_purgatory = 1;
+
 #include <linux/kexec.h>
+#include <linux/libfdt_env.h>
 #include <linux/of_fdt.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
@@ -29,14 +33,99 @@ void (*kexec_hardboot_hook)(void);
 #endif
 
 /**
+ * kexec_is_kernel - Helper routine to check the kernel header signature.
+ */
+static bool kexec_is_kernel(const void *image)
+{
+	struct arm64_image_header {
+		uint8_t pe_sig[2];
+		uint16_t branch_code[3];
+		uint64_t text_offset;
+		uint64_t image_size;
+		uint8_t flags[8];
+		uint64_t reserved_1[3];
+		uint8_t magic[4];
+		uint32_t pe_header;
+	} h;
+
+        if (copy_from_user(&h, image, sizeof(struct arm64_image_header)))
+		return false;
+
+	if (!h.text_offset)
+		return false;
+
+	return (h.magic[0] == 'A'
+		&& h.magic[1] == 'R'
+		&& h.magic[2] == 'M'
+		&& h.magic[3] == 0x64U);
+}
+
+/**
+ * kexec_find_kernel_seg - Helper routine to find the kernel segment.
+ */
+static const struct kexec_segment *kexec_find_kernel_seg(
+	const struct kimage *kimage)
+{
+	int i;
+
+	for (i = 0; i < kimage->nr_segments; i++) {
+		if (kexec_is_kernel(kimage->segment[i].buf))
+			return &kimage->segment[i];
+	}
+
+	BUG();
+	return NULL;
+}
+
+/**
  * kexec_is_dtb - Helper routine to check the device tree header signature.
  */
 static bool kexec_is_dtb(const void *dtb)
 {
 	__be32 magic;
 
-	return get_user(magic, (__be32 *)dtb) ? false :
-		(be32_to_cpu(magic) == OF_DT_HEADER);
+	if (get_user(magic, (__be32 *)dtb))
+		return false;
+
+	return fdt32_to_cpu(magic) == OF_DT_HEADER;
+}
+
+/**
+ * kexec_find_dtb_seg - Helper routine to find the dtb segment.
+ */
+static const struct kexec_segment *kexec_find_dtb_seg(
+	const struct kimage *kimage)
+{
+	int i;
+
+	for (i = 0; i < kimage->nr_segments; i++) {
+		if (kexec_is_dtb(kimage->segment[i].buf))
+			return &kimage->segment[i];
+	}
+
+	BUG();
+	return NULL;
+}
+
+static struct bypass {
+	unsigned long kernel;
+	unsigned long dtb;
+} bypass;
+
+static void fill_bypass(const struct kimage *kimage)
+{
+	const struct kexec_segment *seg;
+
+	seg = kexec_find_kernel_seg(kimage);
+	BUG_ON(!seg || !seg->mem);
+	bypass.kernel = seg->mem;
+
+	seg = kexec_find_dtb_seg(kimage);
+	BUG_ON(!seg || !seg->mem);
+	bypass.dtb = seg->mem;
+
+	pr_debug("%s: kernel: %016lx\n", __func__, bypass.kernel);
+	pr_debug("%s: dtb:    %016lx\n", __func__, bypass.dtb);
 }
 
 /**
@@ -82,10 +171,6 @@ void machine_kexec_cleanup(struct kimage *image)
  */
 int machine_kexec_prepare(struct kimage *image)
 {
-<<<<<<< HEAD
-	arm64_kexec_kimage_start = image->start;
-	kexec_image_info(image);
-=======
 	unsigned long *hardboot_page;
 	kexec_image_info(image);
 	fill_bypass(image);
@@ -105,7 +190,6 @@ int machine_kexec_prepare(struct kimage *image)
 	pr_info("Last hardboot status: %lx\n", hardboot_page[0]);
 	iounmap(hardboot_page);
 
->>>>>>> 289db3581... Implement kexec-hardboot
 	return 0;
 }
 
@@ -303,6 +387,8 @@ void machine_kexec(struct kimage *image)
 	 * and then will transfer control to the entry point of the new kernel.
 	 */
 	soft_restart(reboot_code_buffer_phys);
+
+	BUG(); /* Should never get here. */
 }
 
 void machine_crash_shutdown(struct pt_regs *regs)
--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -432,6 +432,9 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BIOSHOCK)
+extern struct cpufreq_governor cpufreq_gov_bioshock;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bioshock)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
