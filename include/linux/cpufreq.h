116abef347163b31d9506587e0a8548baa57b615
MoreSushant48 <moresushant48@gmail.com>
2016-04-15 11:50:43 +0530

    cpufreq: add Intel Gov

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -468,6 +468,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_GOV_INTEL)
+extern struct cpufreq_governor cpufreq_gov_intel;
+#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_intel)
 #endif
 
 /*********************************************************************
